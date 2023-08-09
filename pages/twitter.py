66ebbc80727f9072383ea963d40830eb0e245841
Dandr11 <113426209+Dandr11@users.noreply.github.com>
2023-08-09 10:28:38 +0300

    Update twitter.py

--- pages/twitter.py
+++ pages/twitter.py
@@ -5,7 +5,8 @@ c = twint.Config() # Настроить
 #c.Lang = "en" 
 #c.Hide_output = True
 cut1= st.selectbox('Интервал2:', ['elonmusk','VitalikButerin','BarrySilbert'])
-c.Username = cut1 
+c.Search = "#python"
+#c.Username = cut1 
 #c.Search = ['India','bjp'] 
 c.Limit = 10 
 st.write(twint.run.Search(c))# Запустить
