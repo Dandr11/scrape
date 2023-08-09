d232919ae1038b8f6f8cd3a147794cfe6b11209c
Dandr11 <113426209+Dandr11@users.noreply.github.com>
2023-08-09 10:21:00 +0300

    Update twitter.py

--- pages/twitter.py
+++ pages/twitter.py
@@ -1,9 +1,11 @@
 import twint
-
+import streamlit as st
+st.set_page_config(layout='wide')
 c = twint.Config() # Настроить
-c.Lang = "en" 
-c.Hide_output = True
-c.Username = "narendramodi" 
-c.Search = ['India','bjp'] 
+#c.Lang = "en" 
+#c.Hide_output = True
+cut1= st.selectbox('Интервал2:', ['elonmusk','VitalikButerin','BarrySilbert')
+c.Username = cut1 
+#c.Search = ['India','bjp'] 
 c.Limit = 10 
-twint.run.Search(c)# Запустить
+st.write(twint.run.Search(c))# Запустить
