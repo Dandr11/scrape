9e83dac65759eb72183d782765e4776ef978bbfc
Dandr11 <113426209+Dandr11@users.noreply.github.com>
2023-08-09 10:09:29 +0300

    Update twitter.py

--- pages/twitter.py
+++ pages/twitter.py
@@ -3,7 +3,7 @@ import twint
 c = twint.Config() # Настроить
 c.Lang = "en" 
 c.Hide_output = True
- c.Username = "narendramodi" 
+c.Username = "narendramodi" 
 c.Search = ['India','bjp'] 
 c.Limit = 10 
 twint.run.Search(c)# Запустить
