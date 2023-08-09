d74881d717fe7b10ab41f8df1dff8002c3feab5f
Dandr11 <113426209+Dandr11@users.noreply.github.com>
2023-08-09 09:40:31 +0300

    Create scrape.py

---
+++ scrape.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Dandr11", page_icon="👋",)
+#st.write("# Это сделал Dandr11! 👋")
+st.markdown("""
+    Streamlit is cool!!!
+    ### Перейти👈
+    - GITHUB (https://github.com/Dandr11/AAD/edit/main/AAD.py)
+    - GITHUB (https://github.com/Dandr11/AAD/blob/main/pages/Binance.py)
+                """)
+#st.balloons()
