85871c24b0b12200cae67911181ef4be73ecdeb7
Dandr11 <113426209+Dandr11@users.noreply.github.com>
2023-08-09 09:34:50 +0300

    Create twitter.py

---
+++ pages/twitter.py
@@ -0,0 +1,10 @@
+import twint
+# Настроить
+ c = twint.Config() 
+c.Lang = "en" 
+c.Hide_output = True
+ c.Username = "narendramodi" 
+c.Search = ['India','bjp'] 
+c.Limit = 1 
+# Запустить
+ twint. запустить.Поиск(с)
